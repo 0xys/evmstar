d13d35e98c3434777486b18fae18131470fdc838
Yushi Sato <yushi.kinger@gmail.com>
2022-02-27 21:27:50 +0900

    basic call test passed

--- tests/call.rs
+++ tests/call.rs
@@ -23,8 +23,8 @@ use evmstar::model::{
 };
 
 fn default_address() -> Address { Address::from_low_u64_be(0xffffeeee) }
-fn consumed_gas(amount: i64) -> i64 {
-    i64::max_value() - amount
+fn consumed_gas(amount: i64, gas_limit: i64) -> i64 {
+    gas_limit - amount
 }
 fn get_address(address_hex: &str) -> Address {
     let mut dst = [0u8; 20];
@@ -69,7 +69,7 @@ fn get_code_for_call(gas: i64, address: &str, value: usize, args_offset: u8, arg
         .append(OpCode::PUSH32)
         .append(U256::from(gas))
         .append(OpCode::CALL);
-    code.clone()
+    code.clone()    // 3 * 7 + 2600 = 2621
 }
 
 #[test]
@@ -79,44 +79,54 @@ fn test_call() {
     let contract_address = "cc000001";
     let mut builder = Code::builder();
     let contract = builder
-        .append(OpCode::PUSH1)
-        .append(1)
-        .append(OpCode::PUSH1)
+        .append(OpCode::PUSH1)  // 3
+        .append(0x00)
+        .append(OpCode::CALLDATALOAD)   // 3
+        .append(OpCode::PUSH1)  // 3
         .append(2)
-        .append(OpCode::ADD)
-        .append(OpCode::PUSH1)
+        .append(OpCode::ADD)    // 3
+        .append(OpCode::PUSH1)  // 3
         .append(0x00)
-        .append(OpCode::MSTORE)
-        .append(OpCode::PUSH1)
+        .append(OpCode::MSTORE) // 3 + 3 = 6
+        .append(OpCode::PUSH1)  // 3
         .append(0x20)
-        .append(OpCode::PUSH1)
+        .append(OpCode::PUSH1)  // 3
         .append(0x00)
-        .append(OpCode::RETURN)
-        .clone();
+        .append(OpCode::RETURN) // 0
+        .clone(); // = 27
 
     host.debug_deploy_contract(contract_address, contract, U256::zero());
     
     let mut builder = Code::builder();
     let code = builder
-        .append(OpCode::PUSH1)
-        .append(1)
-        .append(OpCode::PUSH1)
-        .append(1)
-        .append_code(&mut get_code_for_call(50_000, contract_address, 0, 0, 0, 0, 32))
-        .append(OpCode::PUSH1)
-        .append(0x20)
-        .append(OpCode::PUSH1)
+        .append(OpCode::PUSH1)  // 3
+        .append(0xa0)
+        .append(OpCode::PUSH1)  // 3
         .append(0x00)
-        .append(OpCode::RETURN);
+        .append(OpCode::MSTORE) // 3 + 3 = 6
+        .append_code(&mut get_code_for_call(50_000, contract_address, 0, 0, 0x20, 0, 0x20))
+            // call cost = 2621
+            // execution cost = 27
+            // = 2648
+        .append(OpCode::PUSH1)  // 3
+        .append(0x20)
+        .append(OpCode::PUSH1)  // 3
+        .append(0x00)
+        .append(OpCode::RETURN) // 0
+        .clone();
     
+    let gas_limit = 100_000;
     let mut scope = CallScope::default();
-    scope.code = code.clone();
+    scope.code = code;
     scope.to = default_address();
+    scope.gas_limit = gas_limit;
+    scope.gas_left = gas_limit;
     
     let mut executor = Executor::new_with_tracing(Box::new(host));
     let output = executor.execute_raw_with(scope);
-    let data = decode("0000000000000000000000000000000000000000000000000000000000000003").unwrap();
+    let data = decode("00000000000000000000000000000000000000000000000000000000000000a2").unwrap();
 
     assert_eq!(StatusCode::Success, output.status_code);
     assert_eq!(Bytes::from(data), output.data);
+    assert_eq!(2666, consumed_gas(output.gas_left, gas_limit));
 }
