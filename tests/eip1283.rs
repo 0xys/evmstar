aa27d36632abfd2687dbcc0d4d933e235914d322
0xys <yushi.kinger@gmail.com>
2022-03-22 23:18:20 +0900

    simplify eip1283 test

--- tests/eip1283.rs
+++ tests/eip1283.rs
@@ -1,34 +1,19 @@
-use std::rc::Rc;
-use std::cell::RefCell;
-
-use bytes::Bytes;
 use ethereum_types::{U256, Address};
 
-use evmstar::host::stateful::{
-    StatefulHost,
-};
-use evmstar::executor::{
-    callstack::CallScope,
-    executor::Executor,
-};
-#[allow(unused_imports)]
 use evmstar::model::{
     code::{
         Code, Append,
     },
-    opcode::OpCode,
     evmc::{
-        StatusCode, FailureKind,
+        StatusCode,
         TxContext,
     },
     revision::Revision,
 };
+use evmstar::tester::EvmTester;
 
 fn default_address() -> Address { Address::from_low_u64_be(0xffffeeee) }
 
-fn consumed_gas(amount: i64) -> i64 {
-    i64::max_value() - amount
-}
 fn get_default_context() -> TxContext {
     TxContext {
         gas_price: U256::from(0x1234),
@@ -43,336 +28,140 @@ fn get_default_context() -> TxContext {
     }
 }
 
+fn test_common(code: &str, consumed_gas: i64, gas_refund: i64) {
+    let mut builder = Code::builder();
+    let code = builder.append(code).clone();
+    let gas_limit = i64::max_value();
+
+    let mut tester = EvmTester::new_with(get_default_context());
+    let result = tester.with_to(default_address())
+        .with_gas_limit(gas_limit)
+        .with_gas_left(gas_limit)
+        .run_code_as(code, Revision::Constantinople);
+    
+    result.expect_status(StatusCode::Success)
+        .expect_output("")
+        .expect_gas(consumed_gas)
+        .expect_gas_refund(gas_refund);
+}
+
+fn test_common_with_initial_state(code: &str, consumed_gas: i64, gas_refund: i64) {
+    let mut builder = Code::builder();
+    let code = builder.append(code).clone();
+    let gas_limit = i64::max_value();
+
+    let mut tester = EvmTester::new_with(get_default_context());
+    let result = tester.with_to(default_address())
+        .with_gas_limit(gas_limit)
+        .with_gas_left(gas_limit)
+        .with_storage(default_address(), U256::zero(), U256::from(0x01))
+        .with_to(default_address())
+        .run_code_as(code, Revision::Constantinople);
+    
+    result.expect_status(StatusCode::Success)
+        .expect_output("")
+        .expect_gas(consumed_gas)
+        .expect_gas_refund(gas_refund);
+}
 
 #[test]
 /// test case from: https://eips.ethereum.org/EIPS/eip-1283
 fn test_eip1283_1(){// 1
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let mut builder = Code::builder();
-
-    let code = builder.append("60006000556000600055");
-    let output = executor.execute_raw(&code);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(412, consumed_gas(output.gas_left));
-    assert_eq!(0, output.gas_refund);
+    test_common("60006000556000600055", 412, 0);
 }
 
 #[test]
 fn test_eip1283_2(){// 2
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let mut builder = Code::builder();
-
-    let code = builder.append("60006000556001600055");
-    let output = executor.execute_raw(&code);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(20212, consumed_gas(output.gas_left));
-    assert_eq!(0, output.gas_refund);
+    test_common("60006000556001600055", 20212, 0);
 }
 
 #[test]
 fn test_eip1283_3(){// 3
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let mut builder = Code::builder();
-
-    let code = builder.append("60016000556000600055");
-    let output = executor.execute_raw(&code);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(20212, consumed_gas(output.gas_left));
-    assert_eq!(19800, output.gas_refund);
+    test_common("60016000556000600055", 20212, 19800);
 }
 
 #[test]
 fn test_eip1283_4(){// 4
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let mut builder = Code::builder();
-
-    let code = builder.append("60016000556002600055");
-    let output = executor.execute_raw(&code);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(20212, consumed_gas(output.gas_left));
-    assert_eq!(0, output.gas_refund);
+    test_common("60016000556002600055", 20212, 0);
 }
 
 #[test]
 fn test_eip1283_5(){// 5
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let mut builder = Code::builder();
-
-    let code = builder.append("60016000556001600055");
-    let output = executor.execute_raw(&code);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(20212, consumed_gas(output.gas_left));
-    assert_eq!(0, output.gas_refund);
+    test_common("60016000556001600055", 20212, 0);
 }
 
 #[test]
 fn test_eip1283_6(){// 6
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x01));
-    
-    let mut builder = Code::builder();
-    let code = builder.append("60006000556000600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
-
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(5212, consumed_gas(output.gas_left));
-    assert_eq!(15000, output.gas_refund);
+    test_common_with_initial_state("60006000556000600055", 5212, 15000);
 }
 
 #[test]
 fn test_eip1283_7(){// 7
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x01));
-
-    let mut builder = Code::builder();
-    let code = builder.append("60006000556001600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
-
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(5212, consumed_gas(output.gas_left));
-    assert_eq!(4800, output.gas_refund);
+    test_common_with_initial_state("60006000556001600055", 5212, 4800);
 }
 
 #[test]
 fn test_eip1283_8(){// 8
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x01));
-
-    let mut builder = Code::builder();
-    let code = builder.append("60006000556002600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
-
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(5212, consumed_gas(output.gas_left));
-    assert_eq!(0, output.gas_refund);
+    test_common_with_initial_state("60006000556002600055", 5212, 0);
 }
 
 #[test]
 fn test_eip1283_9(){// 9
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x01));
-
-    let mut builder = Code::builder();
-    let code = builder.append("60026000556000600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
-
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(5212, consumed_gas(output.gas_left));
-    assert_eq!(15000, output.gas_refund);
+    test_common_with_initial_state("60026000556000600055", 5212, 15000);
 }
 
 #[test]
 fn test_eip1283_10(){// 10
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x01));
-
-    let mut builder = Code::builder();
-    let code = builder.append("60026000556003600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
-
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(5212, consumed_gas(output.gas_left));
-    assert_eq!(0, output.gas_refund);
+    test_common_with_initial_state("60026000556003600055", 5212, 0);
 }
 
 #[test]
 fn test_eip1283_11(){// 11
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x01));
-
-    let mut builder = Code::builder();
-    let code = builder.append("60026000556001600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
-
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(5212, consumed_gas(output.gas_left));
-    assert_eq!(4800, output.gas_refund);
+    test_common_with_initial_state("60026000556001600055", 5212, 4800);
 }
 
 #[test]
 fn test_eip1283_12(){// 12
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x01));
-
-    let mut builder = Code::builder();
-    let code = builder.append("60026000556002600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
-
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(5212, consumed_gas(output.gas_left));
-    assert_eq!(0, output.gas_refund);
+    test_common_with_initial_state("60026000556002600055", 5212, 0);
 }
 
 #[test]
 fn test_eip1283_13(){// 13
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x01));
-
-    let mut builder = Code::builder();
-    let code = builder.append("60016000556000600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
-
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(5212, consumed_gas(output.gas_left));
-    assert_eq!(15000, output.gas_refund);
+    test_common_with_initial_state("60016000556000600055", 5212, 15000);
 }
 
 #[test]
 fn test_eip1283_14(){// 14
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x01));
-
-    let mut builder = Code::builder();
-    let code = builder.append("60016000556002600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
-
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(5212, consumed_gas(output.gas_left));
-    assert_eq!(0, output.gas_refund);
+    test_common_with_initial_state("60016000556002600055", 5212, 0);
 }
 
 #[test]
 fn test_eip1283_15(){// 15
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x01));
-
-    let mut builder = Code::builder();
-    let code = builder.append("60016000556001600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
-
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(412, consumed_gas(output.gas_left));
-    assert_eq!(0, output.gas_refund);
+    test_common_with_initial_state("60016000556001600055", 412, 0);
 }
 
 #[test]
 fn test_eip1283_16(){// 16
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x00));
-
+    let code = "600160005560006000556001600055";
     let mut builder = Code::builder();
-    let code = builder.append("600160005560006000556001600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
+    let code = builder.append(code).clone();
+    let gas_limit = i64::max_value();
 
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(40218, consumed_gas(output.gas_left));
-    assert_eq!(19800, output.gas_refund);
+    let mut tester = EvmTester::new_with(get_default_context());
+    let result = tester.with_to(default_address())
+        .with_gas_limit(gas_limit)
+        .with_gas_left(gas_limit)
+        .with_storage(default_address(), U256::zero(), U256::from(0x00))
+        .with_to(default_address())
+        .run_code_as(code, Revision::Constantinople);
+    
+    result.expect_status(StatusCode::Success)
+        .expect_output("")
+        .expect_gas(40218)
+        .expect_gas_refund(19800);
 }
 
 #[test]
 fn test_eip1283_17(){// 17
-    let host = StatefulHost::new_with(get_default_context());
-    let host = Rc::new(RefCell::new(host));
-    (*host).borrow_mut().debug_set_storage(default_address(), U256::zero(), U256::from(0x01));
-
-    let mut builder = Code::builder();
-    let code = builder.append("600060005560016000556000600055");
-    let mut context = CallScope::default();
-    context.code = code.clone();
-    context.to = default_address();
-
-    let mut executor = Executor::new_with(host.clone(), true, Revision::Constantinople);
-    let output = executor.execute_raw_with(context);
-
-    assert_eq!(StatusCode::Success, output.status_code);
-    assert_eq!(Bytes::default(), output.data);
-    assert_eq!(10218, consumed_gas(output.gas_left));
-    assert_eq!(19800, output.gas_refund);
+    test_common_with_initial_state("600060005560016000556000600055", 10218, 19800);
 }
