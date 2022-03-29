3901e3a1feb8ba211736a06722253dd6976ff997
Yushi Sato <yushi.kinger@gmail.com>
2022-03-30 01:33:51 +0900

    add test to revert at deep callscope

--- tests/revert.rs
+++ tests/revert.rs
@@ -118,7 +118,10 @@ fn scope_code(index: u64) -> Code {
 fn address(num: u64) -> Address {
     Address::from_low_u64_be(0xeeeeee00 + num)
 }
-fn call_code(address_u64: u64) -> Code {
+fn address_value(num: u64) -> U256 {
+    U256::from(0xeeeeee00 + num)
+}
+fn call_code_and_add(address_u64: u64) -> Code {
     let code = Code::builder()
         .append(OpCode::PUSH1)
         .append(0x20)   // ret_size
@@ -157,7 +160,7 @@ fn test_multiple_revert() {
     let max = 12;
 
     for i in 0..max {
-        code.append_code(&mut call_code(i as u64));
+        code.append_code(&mut call_code_and_add(i as u64));
     }
     let code = code
         .append(OpCode::PUSH1)
@@ -187,4 +190,187 @@ fn test_multiple_revert() {
     }
 
     result.expect_output("0000000000000000000000000000000000000000000000000000000000000042"); // 0x42 = 66 = 0 + 1 + 2 + ... + 11)
+}
+
+fn call_code(address_u64: u64) -> Code {
+    let code = Code::builder()
+        .append(OpCode::PUSH1)
+        .append(0x20)   // ret_size
+        .append(OpCode::PUSH1)
+        .append(0x00)   // ret_offset
+        .append(OpCode::PUSH1)
+        .append(0x00)   // args_size
+        .append(OpCode::PUSH1)
+        .append(0x00)   // args_offset
+        .append(OpCode::PUSH32)
+        .append(U256::from(0x00))   // value
+        .append(OpCode::PUSH20)
+        .append(address(address_u64))   // address
+        .append(OpCode::GAS)    // 2
+        .append(OpCode::CALL)
+        .clone();
+    code
+}
+
+fn return_contract() -> Code {
+    let code = Code::builder()
+        .append(OpCode::ADDRESS)
+        .append(OpCode::PUSH1)
+        .append(0x00)
+        .append(OpCode::SSTORE)
+        .append(OpCode::PUSH1)
+        .append(0x20)
+        .append(OpCode::PUSH1)
+        .append(0x00)
+        .append(OpCode::RETURN)
+        .clone();
+    code
+}
+fn revert_contract() -> Code {
+    let code = Code::builder()
+        .append(OpCode::ADDRESS)
+        .append(OpCode::PUSH1)
+        .append(0x00)
+        .append(OpCode::SSTORE)
+        .append(OpCode::PUSH1)
+        .append(0x20)
+        .append(OpCode::PUSH1)
+        .append(0x00)
+        .append(OpCode::REVERT)
+        .clone();
+    code
+}
+fn callreturn_contract(called_address: u64) -> Code {
+    let code = Code::builder()
+        .append(OpCode::ADDRESS)
+        .append(OpCode::PUSH1)
+        .append(0x00)
+        .append(OpCode::SSTORE)
+        .append_code(&mut call_code(called_address))
+        .append(OpCode::PUSH1)
+        .append(0x20)
+        .append(OpCode::PUSH1)
+        .append(0x00)
+        .append(OpCode::RETURN)
+        .clone();
+    code
+}
+fn callrevert_contract(called_address: u64) -> Code {
+    let code = Code::builder()
+        .append(OpCode::ADDRESS)
+        .append(OpCode::PUSH1)
+        .append(0x00)
+        .append(OpCode::SSTORE)
+        .append_code(&mut call_code(called_address))
+        .append(OpCode::PUSH1)
+        .append(0x20)
+        .append(OpCode::PUSH1)
+        .append(0x00)
+        .append(OpCode::REVERT)
+        .clone();
+    code
+}
+
+#[test]
+fn test_revert_deep() {
+    let mut tester = EvmTester::new_with(get_default_context());
+    tester.with_default_gas()
+        // [call, call, call, return, return, return]
+        //      | 
+        // call + ---> + 1
+        //      |      |
+        //      | call + ---> + 2
+        //      |             |
+        //      |        call + ---> + 3
+        //      |                    |
+        //      |             + <--- + return
+        //      |             |
+        //      |      + <--- + return
+        //      |      |
+        //      + <--- + return
+        .with_contract_deployed2(address(1), callreturn_contract(2), U256::zero())
+        .with_contract_deployed2(address(2), callreturn_contract(3), U256::zero())
+        .with_contract_deployed2(address(3), return_contract(), U256::zero())
+
+        // [call, call, call, revert, return, return]
+        //      | 
+        // call + ---> + 4
+        //      |      |
+        //      | call + ---> + 5
+        //      |             |
+        //      |        call + ---> + 6
+        //      |                    |
+        //      |             + <--- + revert
+        //      |             |
+        //      |      + <--- + return
+        //      |      |
+        //      + <--- + return
+        .with_contract_deployed2(address(4), callreturn_contract(5), U256::zero())
+        .with_contract_deployed2(address(5), callreturn_contract(6), U256::zero())
+        .with_contract_deployed2(address(6), revert_contract(), U256::zero())
+
+        // [call, call, call, return, revert, return]
+        //      | 
+        // call + ---> + 7
+        //      |      |
+        //      | call + ---> + 8
+        //      |             |
+        //      |        call + ---> + 9
+        //      |                    |
+        //      |             + <--- + return
+        //      |             |
+        //      |      + <--- + revert
+        //      |      |
+        //      + <--- + return
+        .with_contract_deployed2(address(7), callreturn_contract(8), U256::zero())
+        .with_contract_deployed2(address(8), callrevert_contract(9), U256::zero())
+        .with_contract_deployed2(address(9), return_contract(), U256::zero())
+
+        // [call, call, call, return, return, revert]
+        //      | 
+        // call + ---> + 10
+        //      |      |
+        //      | call + ---> + 11
+        //      |             |
+        //      |        call + ---> + 12
+        //      |                    |
+        //      |             + <--- + return
+        //      |             |
+        //      |      + <--- + return
+        //      |      |
+        //      + <--- + revert
+        //      | 
+        .with_contract_deployed2(address(10), callrevert_contract(11), U256::zero())
+        .with_contract_deployed2(address(11), callreturn_contract(12), U256::zero())
+        .with_contract_deployed2(address(12), return_contract(), U256::zero());
+
+    let code = Code::builder()
+        .append_code(&mut call_code(1))
+        .append_code(&mut call_code(4))
+        .append_code(&mut call_code(7))
+        .append_code(&mut call_code(10))
+        .append(OpCode::PUSH1)
+        .append(0x20)
+        .append(OpCode::PUSH1)
+        .append(0x00)
+        .append(OpCode::RETURN)
+        .clone();
+
+    let result = tester.run_code(code);
+    result.expect_status(StatusCode::Success)
+        .expect_storage(address(1), U256::zero(), address_value(1))
+        .expect_storage(address(2), U256::zero(), address_value(2))
+        .expect_storage(address(3), U256::zero(), address_value(3))
+
+        .expect_storage(address(4), U256::zero(), address_value(4))
+        .expect_storage(address(5), U256::zero(), address_value(5))
+        .expect_storage(address(6), U256::zero(), U256::zero())
+
+        .expect_storage(address(7), U256::zero(), address_value(7))
+        .expect_storage(address(8), U256::zero(), U256::zero())
+        .expect_storage(address(9), U256::zero(), U256::zero())
+
+        .expect_storage(address(10), U256::zero(), U256::zero())
+        .expect_storage(address(11), U256::zero(), U256::zero())
+        .expect_storage(address(12), U256::zero(), U256::zero());
 }
